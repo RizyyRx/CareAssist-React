4eead9b70e7ccfdae5382fbbffaf9991c2d74c0c
RizyyRx <rizwankendo@gmail.com>
2025-11-07 11:13:56 +0530

    Updated catch statment to handle objects

--- src/pages/UpdatePatientProfile.jsx
+++ src/pages/UpdatePatientProfile.jsx
@@ -44,11 +44,20 @@ function UpdatePatientProfile() {
         try{
             const result = await axios.put("http://localhost:8080/api/patient/update-profile", formData,{headers:{Authorization:`Bearer ${token}`}})
             setMessage(result.data)
-        } catch(error){
-            if(error.response){
-                setMessage(error.response.data);
+        } catch (error) {
+            if (error.response) {
+                const data = error.response.data;
+
+                if (typeof data === "object") {
+                    const formatted = Object.entries(data)
+                        .map(([key, value]) => `${key}: ${value}`)
+                        .join(", ");
+                    setMessage(formatted);
+                } else {
+                    setMessage(data);
+                }
             } else {
-                setMessage("Server not reachable")
+                setMessage("Server not reachable");
             }
         }
     }
@@ -57,19 +66,20 @@ function UpdatePatientProfile() {
     <div>
         <h1>Update Patient Profile</h1>
         <form onSubmit={handleSubmit}>
-            <input type='text' name='firstName' placeholder='First Name' value={formData.firstName} onChange={handleChange}></input>
-            <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} />
-            <input type="date" name="dob" placeholder="Date of Birth" value={formData.dob} onChange={handleChange} />
+            <input type='text' name='firstName' placeholder='First Name' value={formData.firstName} onChange={handleChange}></input><br/>
+            <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} /><br/>
+            <label htmlFor="dob">Date of Birth</label><br/>
+            <input type="date" name="dob" placeholder="Date of Birth" value={formData.dob} onChange={handleChange} /><br/>
             <select name='gender' value={formData.gender} onChange={handleChange}>
                 <option value="">Select gender</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
                 <option value="preferNotToSay">Prefer not to say</option>
-            </select>
-            <input type="number" name="contactNumber" placeholder="Contact Number" value={formData.contactNumber} onChange={handleChange} />
-            <input type="text" name="address" placeholder="Address" value={formData.address} onChange={handleChange} />
-            <input type="text" name="medicalHistory" placeholder="Medical History" value={formData.medicalHistory} onChange={handleChange} />
+            </select><br/>
+            <input type="number" name="contactNumber" placeholder="Contact Number" value={formData.contactNumber} onChange={handleChange} /><br/>
+            <input type="text" name="address" placeholder="Address" value={formData.address} onChange={handleChange} /><br/>
+            <input type="text" name="medicalHistory" placeholder="Medical History" value={formData.medicalHistory} onChange={handleChange} /><br/>
             <button type="submit">Update</button>
         </form>
         {message && <p>{message}</p>}
